ee9409ab606f3d5a17f6fa04d07d14a1a6c7e90d
JonathanCollaud <jonathan.collaud@epfl.ch>
2020-03-19 14:59:43 +0100

    Database, pictures and icons

---
+++ Pokemon_world/Icons/scrap_pictures.py
@@ -0,0 +1,13 @@
+import urllib
+import requests
+
+filename = "images"
+
+for i in range(1, 803):
+    url = 'https://www.pokemontrash.com/pokedex/images/minixy/'
+    img_data = requests.get(url +'%03d'%i + '.png').content
+    with open('%03d'%i +'.jpg', 'wb') as handler:
+        handler.write(img_data)
+    print ('Image saved for %03d.png'%i)
+
+    
