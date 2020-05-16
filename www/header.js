6188eb9a463049e0d83bc759e239d98d3fe67e58
Stache <hugo.hueber@epfl.ch>
2020-05-16 18:06:54 +0200

    Work in progress, added swag

--- www/header.js
+++
@@ -1,8 +0,0 @@
-function loadHeader() {
-		<ul class="width-limited">
-			<li><a href="index.html">Home</a></li>
-			<li><a href="chart.html">Pokemon vizualization</a></li>
-			<li><a href="data_table.html">Data Table</a></li>
-			<li><a href="team.html">Team</a></li>
-		</ul>
-}
